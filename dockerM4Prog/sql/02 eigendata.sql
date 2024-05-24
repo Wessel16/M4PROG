5b277117d63bc0dce0b922d8980c8925a194b2fa
Wessel van Arkel <36850@ma-web.nl>
2024-05-24 10:07:34 +0200

    Les 01 en Les 02 gemaakt

---
+++ dockerM4Prog/sql/02 eigendata.sql
@@ -0,0 +1,3 @@
+SELECT merk, model, kilometerstand, verbruik, prijs FROM Auto;
+SELECT model, merk, verbruik, kilometerstand, prijs FROM Auto;
+SELECT kilometerstand, model, prijs, merk, verbruik FROM Auto
---
+++ dockerM4Prog/sql/02 huisdierselect.sql
@@ -0,0 +1,5 @@
+SELECT naam FROM `huisdieren`;
+
+SELECT * FROM `huisdieren`;
+
+SELECT typedier, leeftijd, geboortedatum FROM `huisdieren`;
